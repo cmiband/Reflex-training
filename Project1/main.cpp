ee719358796a8164ceafbe429d5b3ab32adc07bf
XCreckerX <33941168+XCreckerX@users.noreply.github.com>
2021-07-19 18:58:12 +0200

    Changed cursor and background

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,20 +1,38 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
 #include <iostream>
 
 using namespace std; 
 
 int main() {
-	sf::RenderWindow window(sf::VideoMode(800, 600), "Mikoshi");
+	sf::RenderWindow window(sf::VideoMode(1000, 800), "Reflex training");
 
-	sf::Event event;
+	sf::Color bgColor(204,255,255);
 
+	sf::Cursor cursor;
+	if (!cursor.loadFromSystem(sf::Cursor::Cross))
+		return EXIT_FAILURE;
+
+	sf::RectangleShape titleRec(sf::Vector2f(200.0f, 50.0f));
+	
+	titleRec.setPosition(sf::Vector2f(400.0f, 100.0f));
+
+	titleRec.setFillColor(sf::Color::White);
+
+	window.setMouseCursor(cursor);
+
+	window.clear(bgColor);
 	while (window.isOpen()) {
+		sf::Event event;
 		while (window.pollEvent(event)) {
 			if (event.type == sf::Event::Closed)
 			{
 				window.close();
 			}
 		}
+		window.clear(bgColor);
+
+		window.display();
 	}
 
 	return 0;
