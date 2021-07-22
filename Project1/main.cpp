20aad33b48a5b3ec74ecb76982e222e28ffd797a
XCreckerX <33941168+XCreckerX@users.noreply.github.com>
2021-07-22 17:16:12 +0200

    Project's stem completed
    also experienced strange unhandled exception with std::bad_alloc

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -7,9 +7,11 @@
 using namespace std; 
 
 void ChangePos(sf::RectangleShape& shp) {
-	float rrx = rand() % 970 + 30;
-	float rry = rand() % 780 + 20;
-	sf::Vector2f newPos = sf::Vector2f(rrx, rry);
+	int rrx = rand() % 970 + 30;
+	int rry = rand() % 780 + 20;
+	float frrx = static_cast<float>(rrx);
+	float frry = static_cast<float>(rry);
+	sf::Vector2f newPos = sf::Vector2f(frrx, frry);
 	shp.setPosition(newPos);
 }
 
@@ -23,17 +25,14 @@ int main() {
 	if (!cursor.loadFromSystem(sf::Cursor::Cross))
 		return EXIT_FAILURE;
 
-	int hits = 0;
-	int misses = 0;
-
 	sf::Mouse mouse;
 
 	sf::Font font;
 	if (!font.loadFromFile("BebasNeue-Regular.ttf"))
 		return EXIT_FAILURE;
 	sf::Text title("Reflex training", font, 50);
-	sf::Text hitsShow("Hits : 0", font, 30);
-	sf::Text missesShow("Misses : 0", font, 30);
+	sf::Text hitsShow("", font, 30);
+	sf::Text missesShow("", font, 30);
 
 	sf::RectangleShape titleRec(sf::Vector2f(400.0f, 75.0f));
 	sf::RectangleShape objective(sf::Vector2f(40.0f, 40.0f));
@@ -41,12 +40,14 @@ int main() {
 	titleRec.setPosition(sf::Vector2f(300.0f, 25.0f));
 	title.setPosition(sf::Vector2f(370.0f, 30.0f));
 	objective.setPosition(sf::Vector2f(500.0f, 400.0f));
-	hitsShow.setPosition(sf::Vector2f(100.0f, 50.0f));
-	missesShow.setPosition(sf::Vector2f(800.0f, 50.0f));
+	hitsShow.setPosition(sf::Vector2f(50.0f, 40.0f));
+	missesShow.setPosition(sf::Vector2f(850.0f, 40.0f));
 
 	titleRec.setFillColor(sf::Color::White);
 	title.setFillColor(textColor);
 	objective.setFillColor(sf::Color(255, 0, 0));
+	hitsShow.setFillColor(textColor);
+	missesShow.setFillColor(textColor);
 
 	window.setMouseCursor(cursor);
 
@@ -54,27 +55,26 @@ int main() {
 
 	sf::Clock clock;
 	int moveIterator = 0;
+	int hits = 0; 
+	int misses = 0;
 	while (window.isOpen()) {
 		sf::Event event;
 		while (window.pollEvent(event)) {
 			if (event.type == sf::Event::Closed)
 			{
 				window.close();
-			}
-			string hits = "Hits : " + hits;
-			string misses = "Misses : " + misses;
-
-
-
+			}			
 			sf::Vector2i mousePos = mouse.getPosition(window);
-			sf::Vector2f mousePosFloat = sf::Vector2f(mousePos.x, mousePos.y);
+			float mx = static_cast<float>(mousePos.x);
+			float my = static_cast<float>(mousePos.y);
+
+			sf::Vector2f mousePosFloat = sf::Vector2f(mx, my);
 
 			bool onTarget = objective.getGlobalBounds().contains(mousePosFloat);
-			cout << onTarget << endl;
-
 
 			sf::Time elapsed = clock.getElapsedTime();
-			int timeApproximated = elapsed.asSeconds();
+			float timeRaw = elapsed.asSeconds();
+			int timeApproximated = static_cast<int>(timeRaw);
 
 			if (timeApproximated % 3 == 0)
 				moveIterator = 0;
@@ -87,18 +87,29 @@ int main() {
 			if (event.type == sf::Event::MouseButtonPressed) {
 				if (event.mouseButton.button == sf::Mouse::Left && onTarget) {
 					ChangePos(objective);
+					hits++;
 				}
+				else if (event.mouseButton.button == sf::Mouse::Left && !onTarget)
+					misses++;
 			}
+
+			string hitsString = to_string(hits);
+			string missesString = to_string(misses);
+			string hitsShowString = "Hits : " + hitsString;
+			string missesShowString = "Misses : " + missesString;
+			hitsShow.setString(hitsShowString);
+			missesShow.setString(missesShowString);
 
 			window.clear(bgColor);
 
 			window.draw(titleRec);
 			window.draw(title);
 			window.draw(objective);
+			window.draw(hitsShow);
+			window.draw(missesShow);
 
 			window.display();
 		}
 	}
-
 	return 0;
 }
